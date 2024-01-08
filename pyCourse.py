d993463b834c7f1ee0be7bcdaefe7e94b7a7d39a
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-08 21:17:01 +0000

    added ex with for loops

--- pyCourse.py
+++ pyCourse.py
@@ -54,3 +54,39 @@ print(d['k1'][0]['nest_key'][1][0])
 d = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
 print(d['k1'][2]['k2'][1]['tough'][2][0])
 
+
+# for loops
+
+myList = [1, 2, 3, 4, 5]
+for n in myList:
+    print('yupy')
+
+# iterate without a variable
+for _ in "hello":
+    print("great")
+
+# tuple unpacking - duplicate the items of the sequence and unpack them
+myList = [(1,2), (3, 4), (5, 6)]
+for (a, b) in myList:
+    print(a)
+    print(b)
+
+for a, b in myList:
+    print(b)
+
+d = {'k1': 1, 'k2': 2, 'k3': 3}
+
+# iterate through the keys, by default iterating through a dict
+for item in d:
+    print(item)
+
+# iterate through the items
+for _ in d.items():
+    print(_)
+
+for key, value in d.items():
+    print(key)
+
+for value in d.values():
+    print(value)
+
