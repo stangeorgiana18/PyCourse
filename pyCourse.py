6ddbfea0091163f4988ed41305253fe30d7678d3
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-08 08:52:06 +0000

    added ex

--- pyCourse.py
+++ pyCourse.py
@@ -32,3 +32,24 @@ with open('createdFile.txt', 'w') as f:
 with open('createdFile.txt', 'r') as f:
     print(f.read())
 
+# ex with data structures
+    
+list3 = [1,2,[3,4,'hello']]
+list3[2][2] = 'goodbye'
+print(list3)
+
+list4 = [5,3,4,6,1]
+list4.sort()
+print(list4)
+
+d = {'simple_key':'hello'}
+print(d['simple_key'])
+
+d = {'k1':{'k2':'hello'}}
+print(d['k1']['k2'])
+
+d = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+print(d['k1'][0]['nest_key'][1][0])
+
+d = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
+print(d['k1'][2]['k2'][1]['tough'][2][0])
