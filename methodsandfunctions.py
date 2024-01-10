687737d64d23bd2df28c54649e977b4f9aeca3ba
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-10 17:23:59 +0000

    methods

---
+++ methodsandfunctions.py
@@ -0,0 +1,14 @@
+# methods = functions that are built into objects
+
+# methods in built-in objects:
+myList = [1, 2, 3]
+myList.append(4)
+print(myList)
+print(myList.pop())
+
+# control + space -> methods available for the object
+
+# get the documentation of the object
+help(myList.insert)                                
+
+
