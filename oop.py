d92575abbd108ecddd3c940ff613d98b6e3cf36c
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-15 20:32:10 +0000

    method affecting attribute

--- oop.py
+++ oop.py
@@ -309,5 +309,28 @@ coordinate2 = (8, 10)
 li = Line(coordinate1, coordinate2)
 
 print(li.distance())
-print(li.slope())
+print(li.slope(), '\n')
+
+
+# methods that affect attributes 
+
+class Simple():
+
+    def __init__(self, value):
+        
+        self.value = value
+    
+    def add_to_value(self, amount):
+        
+        self.value = self.value + amount
+        print('We just added {} to your value'.format(amount))
+
+    
+myobj = Simple(300)
+
+print(myobj.value)
+
+print(myobj.add_to_value(500))
+print(myobj.value)
+
 
