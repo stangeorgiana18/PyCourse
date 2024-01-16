3573df4e649e1488414a3d4ac41070e7f24a65ef
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-16 17:52:04 +0000

    logic behind name, main

--- one.py
+++ one.py
@@ -3,9 +3,13 @@
 def func():
     print("Func() in one.py")
 
-print("Top level in one.py")
+def function():
+    pass
+
+def function2():
+    pass
 
 if __name__ == '__main__':
-    print("one.py is being run directly!")
-else:
-    print("one.py has been imported!")
+   # RUN SCRIPT DIRECTLY
+    function2()
+    function()
