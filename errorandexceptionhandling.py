88944ebec481268cb98e97afeb8f9b223fcd8072
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-16 18:09:49 +0000

    corrected

--- errorandexceptionhandling.py
+++ errorandexceptionhandling.py
@@ -18,7 +18,7 @@ else:
 
 # see built-in exceptions in the documentation
 try:
-    f = open("testfile", w)
+    f = open("testfile", 'w')
     f.write("Ce interesant")
 except TypeError:
     print("There was a type error!")
@@ -28,4 +28,4 @@ except OSError:
 finally:
     # execute no matter what
     print("I always run.")
-    
+
