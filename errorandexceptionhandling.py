bdcd30ff84b0c9410e892e8d88a2a01ef7e2ec95
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-16 18:01:28 +0000

    except

--- errorandexceptionhandling.py
+++ errorandexceptionhandling.py
@@ -3,24 +3,17 @@
 # finally - executed regardless of an error
 
 
-def add(n1, n2):
-    print(n1 + n2)
-
-add(10, 20)
-
-number1 = 10
-number2 = input("Provide a number: ")
-
-add(number1, number2)
-print("Something happened!")
-
 try:
     # WANT TO ATTEMP THIS CODE
     # MAY HAVE AN ERROR
-    result = 10 + 10
+    result = 10 + '10'
 except:
     # IF THERE'S AN ERROR
     print("It looks like you aren't adding correctly.")
+else:
+    # if there's no exception/error there
+    print("Add went well.")
+    print(result)
 
 
 print(result)
