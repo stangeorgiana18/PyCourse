6a519b724a817888305d4867c304605dd40e6da7
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-16 18:09:01 +0000

    another eg

--- errorandexceptionhandling.py
+++ errorandexceptionhandling.py
@@ -6,7 +6,7 @@
 try:
     # WANT TO ATTEMP THIS CODE
     # MAY HAVE AN ERROR
-    result = 10 + '10'
+    result = 10 + 10
 except:
     # IF THERE'S AN ERROR
     print("It looks like you aren't adding correctly.")
@@ -16,4 +16,16 @@ else:
     print(result)
 
 
-print(result)
+# see built-in exceptions in the documentation
+try:
+    f = open("testfile", w)
+    f.write("Ce interesant")
+except TypeError:
+    print("There was a type error!")
+except OSError:
+    # if opening a file you don't have permission to
+    print("You have an OS Error")
+finally:
+    # execute no matter what
+    print("I always run.")
+    
