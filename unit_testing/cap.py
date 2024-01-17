2124270f7527286c4bdf6dbc57b34e75a430be61
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-17 20:26:44 +0000

    tests run

--- unit_testing/cap.py
+++ unit_testing/cap.py
@@ -4,5 +4,5 @@ def cap_text(text):
     Input a string
     Outut the capitalized string (first letter)
     '''
-    return text.capitalize()
+    return text.title()
 
--- unit_testing/test_cap.py
+++ unit_testing/test_cap.py
@@ -1,11 +1,12 @@
 import unittest
-import cap 
+import cap # import any of the scripts you're working on
 
 class TestCap(unittest.TestCase):
 
     def test_one_word(self):
         text = 'python'
         result = cap.cap_text(text)
+        # the result got from "result" is 'Python'?
         self.assertEqual(result, 'Python')
 
     def test_multiple_words(self):
