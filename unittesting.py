fead45aedc666b37c8e84b7be20dba2e07fc6f67
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-17 19:52:11 +0000

    unit testing demo

---
+++ unittesting.py
@@ -0,0 +1,10 @@
+# pylint library - reports back possible issues, eg. styling issues, invalid code
+# unittest library - built-in, check for desired outputs
+
+a = 1
+b = 2
+print(a)
+print(B)
+
+# to see the report in the terminal:
+# pylint unittesting.py -r y
