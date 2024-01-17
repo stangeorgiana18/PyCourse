5d9d57b25730879609032f9fc4c79ffcca0bda41
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-17 21:36:49 +0000

    first class done

--- warcardsgame.py
+++ warcardsgame.py
@@ -1,12 +1,21 @@
 # create a card class
 # understand the suit, rank, value of the card
 
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
+            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+
+
 # Card() - parantheses not necessary since we're not using inheritance
 class Card:
 
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+        self.value = values[rank]
     
     def __str__(self):
         return self.rank + ' of ' + self.suit
@@ -17,11 +26,13 @@ print(two_hearts)
 
 print(two_hearts.rank, two_hearts.suit)
 
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
-            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
-
-
 print(values[two_hearts.rank])
+
+three_of_clubs = Card("Clubs", "Three")
+
+print(three_of_clubs.value)
+
+print(three_of_clubs.value > three_of_clubs.value)
 
 
 # deck class 
