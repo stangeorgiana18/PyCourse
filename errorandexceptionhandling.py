dd92b87f539e7311e860c7c740e5b67f8345fc9b
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-17 19:31:02 +0000

    practice with try, except, finally

--- errorandexceptionhandling.py
+++ errorandexceptionhandling.py
@@ -29,6 +29,7 @@ finally:
     print("I always run.")
 
 
+
 def ask_for_int():
 
     while True:
@@ -42,3 +43,49 @@ def ask_for_int():
             break
         
 ask_for_int()
+
+print('\n')
+
+# practice ex
+
+try:
+    for i in ['a', 'b', 'c']:
+        print(i ** 2)
+except TypeError:
+    print("Operands should be numbers.")
+
+print('\n')
+
+
+try:
+    x = 5
+    y = 0
+
+    z = x/y
+    print(z)
+except:
+    print("You cannot divide by zero.")
+finally:
+    print("All done.")
+
+print('\n')
+
+
+def ask():
+
+    # waiting for a correct response
+    waiting = True
+    while waiting:
+        try:
+            n = int(input("Choose a number: "))
+            squared_n = n ** 2
+        except:
+            print("An error occurred! Please try again. \n")
+            continue
+        else:
+            waiting = False
+    
+    print("Your number squared is: ")
+    print(n ** 2)
+
+ask()
