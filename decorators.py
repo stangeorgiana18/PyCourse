75b16fa08c57b84490958f6d2cd04fcfed0a20d5
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-19 12:40:38 +0000

    functions defined within another func

--- decorators.py
+++ decorators.py
@@ -8,21 +8,32 @@ print(func())
 def hello():
     return "hello!"
 
-greet = hello
+#greet = hello
 
-print(greet(), '\n')
+#print(greet(), '\n')
 
 # did greet make its own copy of the hello function or just pointed to hello?
 # test by deleting hello and see if we can still call greet
 
 del hello
 
-print(greet())
+#print(greet())
 
-# passing in a function within another funcion
+# passing in a function within another function
 
 def hello(name = "Geo"):
     print("The hello() function has been executed!")
+
+    def greet():
+        return '\t This is a greet() function inside hello!'
+    
+    def welcome():
+        return '\t This is welcome() inside hello.'
+    
+    print(greet())
+    print(welcome())
+    print("This is the end of the hello function!")
+
 
 hello()
 
