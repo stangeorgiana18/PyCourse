d09bd029c9bb29c4ec0ab5669ffb726aa1b1d643
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-21 18:55:29 +0000

    debug by setting trace

--- advancedPyModules/5_pydebugger.py
+++ advancedPyModules/5_pydebugger.py
@@ -1,14 +1,21 @@
-# debugging with print statements
+
+# use the python debugger to set a trace
+
+import pdb
 
 x = [1, 2, 3]
 y = 2
 z = 3
 
-result1 = y + z
-print(result1)
-result2 = x + y
-print(y)
-print(result2)
+# a trace is going to pause operations mid script and allow you to play
+# with variables to understand what's going on
+# SET THE TRACE BEFORE THE ERROR LINE 
+result_one = y + z
 
-# use the python debugger to set a trace
+pdb.set_trace() # => a little interactive environment
+# I can call variables to check what they are 
+# or perform other operations, x + y
+
+result_two = y + x
+
 
