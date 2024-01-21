954882ff72fc7a2e47f2a64df08e073032f0ffb6
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-21 18:48:16 +0000

    debbug using print statements

---
+++ advancedPyModules/5_pydebugger.py
@@ -0,0 +1,14 @@
+# debugging with print statements
+
+x = [1, 2, 3]
+y = 2
+z = 3
+
+result1 = y + z
+print(result1)
+result2 = x + y
+print(y)
+print(result2)
+
+# use the python debugger to set a trace
+
