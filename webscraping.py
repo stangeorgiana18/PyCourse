8fd31150f0a7a5bdd9710092d73757cb218b8398
Georgiana Stan <stangeorgiana18@yahoo.com>
2024-01-24 07:41:01 +0000

    grab a title

---
+++ webscraping.py
@@ -0,0 +1,45 @@
+# techniques involving automating gathering of data from a website
+# main focus: become expert in looking up information and generalizing the process 
+# of web scraping and apply so you can apply it to unique situations
+# idea: poiny py to a particular either CSS tag or HTML element and grab info baseed off that
+
+# BeautifulSoup and requests libraries
+
+import requests
+import bs4
+
+# inspect each source and a particular element
+
+result = requests.get('http://www.example.com')
+print(type(result)) # <class 'requests.models.Response'>
+
+print(result.text, '\n') # the html document
+
+# (text_string, string_code_for_what_engine_to_use_to_parse_through_the_HTML_text)
+soup = bs4.BeautifulSoup(result.text, "lxml") 
+
+print(soup) # raw string --> soup object grabbing things based on tags/elements
+
+title_tags = soup.select('title')
+print(title_tags) # [<title>Example Domain</title>] - list
+
+paragraph_tags = soup.select('p')
+print(paragraph_tags)
+
+# [<p>This domain is for use in illustrative examples in documents. You may use this
+# domain in literature without prior coordination or asking for permission.</p>, <p><a href="https://www.iana.org/domains/example">More information...</a></p>]
+
+
+first_item_text = soup.select('title')[0].getText()
+print(first_item_text) # Example Domain
+
+site_paragraphs = soup.select('p')
+
+print(type(site_paragraphs[0])) # <class 'bs4.element.Tag'>
+# not a string, but a bs object, so I can use getText()
+
+print(site_paragraphs[0].getText()) # first paragraph
+
+
+# GRABBING ALL ELEMENTS OF A CLASS 
+
